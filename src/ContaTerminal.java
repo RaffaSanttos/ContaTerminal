e6ab7094f204e52b01aa021337d6752dc0944214
RaffaSanttos <atomix.luke@gmail.com>
2024-10-16 14:46:54 -0300

    fechando o scanner para evitar vazamento de dados ou bloqueio de recursos

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -20,6 +20,9 @@ public class ContaTerminal {
         double saldo = scanner.nextDouble();  
 
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta "+ numero + " e seu saldo " + saldo  + " já está disponível para saque");
+
+        //fechar o scanner evita vazamento de dados.
+        scanner.close();
     }
 
     public static String mensagem (int numero, String agencia, String nomeCliente, double saldo){
