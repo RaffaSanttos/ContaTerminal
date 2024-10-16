ed741bbc25b7fbda84492891a1a1773ff6c2647b
RaffaSanttos <atomix.luke@gmail.com>
2024-10-16 12:28:29 -0300

    first commit

---
+++ src/ContaTerminal.java
@@ -0,0 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        
+
+        System.out.println("Por favor, o número da conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite sua agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite seu nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o quanto quer depositar: ");
+        double saldo = scanner.nextDouble();  
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta "+ numero + " e seu saldo " + saldo  + " já está disponível para saque");
+    }
+
+    public static String mensagem (int numero, String agencia, String nomeCliente, double saldo){
+        return "Olá ".concat(nomeCliente)
+            .concat(", obrigado por criar uma conta em nosso banco, sua agência é ")
+            .concat(agencia).concat(", conta")
+            .concat(String.valueOf(numero))
+            .concat(" e seu saldo ")
+            .concat(String.valueOf(saldo))
+            .concat(" já está disponível para saque");
+    }
+}
